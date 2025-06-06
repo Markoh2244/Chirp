a09a6f368f4d6a8f7a960338785b1f5a7641bbe1
Mark Henien <markhenien@MacBookPro.lan>
2025-06-06 09:44:55 -0400

    added basic auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 const dynamic = "force-dynamic";
+import { SignedOut,SignedIn } from "@clerk/nextjs"
 
 
-
-export default async function HomePage() {
+async function Images(){
 
   const images = await db.query.images.findMany({
     orderBy: (images, { desc }) => [desc(images.id)],
   });
-  return (
-    <main className ="">
-      <div className = "flex flex-wrap gap-4">
+
+  return (<div className = "flex flex-wrap gap-4">
         {images.map((post) => (<div key={post.id}> {post.name}</div>))}
         {[...images, ...images, ...images, ...images].map((image, idx) => (
           <div key={image.id + "-" + idx} className="w-48">
@@ -19,8 +18,22 @@ export default async function HomePage() {
             <div>{image.name}</div>
           </div>
         ))}
-      </div>
-      Hello (gallery in progress)
+
+      </div>)
+}
+export default async function HomePage() {
+
+  
+  return (
+    <main className ="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
     </main>
   );
 }
